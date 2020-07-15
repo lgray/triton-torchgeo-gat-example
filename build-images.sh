d81e8f2c95c058586080b9449668d2d91542dadb
Lindsey Gray <lindsey.gray@gmail.com>
2020-07-15 18:50:13 -0500

    artifacts and changes for 20.06-v1-py3

--- build-images.sh
+++ build-images.sh
@@ -15,10 +15,15 @@ docker run -d --name triton_server_temp \
        -p8000:8000 -p8001:8001 -p8002:8002 \
        local/gat_test_server:latest
 
-mkdir -p ./models/gat_test/1
+mkdir -p artifacts/models/gat_test/1
+mkdir -p artifacts/lib
 
-cp config.pbtxt models/gat_test/
-docker cp triton_server_temp:/opt/tritonserver/model.pt models/gat_test/1/
+cp config.pbtxt artifacts/models/gat_test/
+docker cp triton_server_temp:/opt/tritonserver/model.pt artifacts/models/gat_test/1/
+docker cp triton_server_temp:/opt/tritonserver/libpyg/libtorchscatter.so artifacts/lib/
+docker cp triton_server_temp:/opt/tritonserver/libpyg/libtorchsparse.so artifacts/lib/
+docker cp triton_server_temp:/opt/tritonserver/libpyg/libtorchcluster.so artifacts/lib/
+docker cp triton_server_temp:/opt/tritonserver/libpyg/libtorchsplineconv.so artifacts/lib/
 
 docker stop triton_server_temp
 docker rm triton_server_temp
