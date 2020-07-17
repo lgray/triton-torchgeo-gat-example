f924100feeb2a56b805d37afd800da010bf6ab9e
Lindsey Gray <lindsey.gray@gmail.com>
2020-07-16 19:33:25 -0500

    use fewer large containers, optimize deployment

--- build-images.sh
+++ build-images.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# build the client container
-docker build -t local/gat_test_client -f Dockerfile.client .
-
 # build necessary libraries and train the example model
-docker build -t local/gat_test_server -f Dockerfile.build .
+docker build -t local/gat_artifacts -f Dockerfile.build .
 
-# build container for running locally
-docker build -t local/gat_local_test -f Dockerfile.local .
+# build container for running locally (uncomment if you want it)
+#docker build -t local/gat_local_test -f Dockerfile.local .
 
-docker run -d --name triton_server_temp \
-       --shm-size=1g --ulimit memlock=-1 \
-       --ulimit stack=67108864 \
-       -p8000:8000 -p8001:8001 -p8002:8002 \
-       local/gat_test_server:latest
+docker run -d --name triton_artifacts \
+       local/gat_artifacts:latest
 
-mkdir -p ./models/gat_test/1
+mkdir -p artifacts/models/gat_test/1
+mkdir -p artifacts/lib
 
-cp config.pbtxt models/gat_test/
-docker cp triton_server_temp:/opt/tritonserver/model.pt models/gat_test/1/
+cp config.pbtxt artifacts/models/gat_test/
+docker cp triton_artifacts:/artifacts/model.pt artifacts/models/gat_test/1/
+docker cp triton_artifacts:/artifacts/libtorchscatter.so artifacts/lib/
+docker cp triton_artifacts:/artifacts/libtorchsparse.so artifacts/lib/
+docker cp triton_artifacts:/artifacts/libtorchcluster.so artifacts/lib/
+docker cp triton_artifacts:/artifacts/libtorchsplineconv.so artifacts/lib/
 
-docker stop triton_server_temp
-docker rm triton_server_temp
+docker stop triton_artifacts
+docker rm triton_artifacts
--- start.sh
+++ start.sh
@@ -7,14 +7,16 @@ docker run -d --name gat_test_server \
        --shm-size=1g --ulimit memlock=-1 \
        --ulimit stack=67108864 \
        -p8000:8000 -p8001:8001 -p8002:8002 \
-       -v`pwd`/models:/models \
-       -e LD_PRELOAD="libpyg/libtorchscatter.so libpyg/libtorchsparse.so" \
-       local/gat_test_server:latest \
-       tritonserver --model-repository=/models
+       -v`pwd`/artifacts:/inputs \
+       -e LD_LIBRARY_PATH="/opt/tritonserver/lib/pytorch:/usr/local/cuda/compat/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64" \
+       -e LD_PRELOAD="/inputs/lib/libtorchscatter.so /inputs/lib/libtorchsparse.so" \
+       nvcr.io/nvidia/tritonserver:20.06-py3 \
+       tritonserver --model-repository=/inputs/models
 
 sleep 2
 
 docker run --name gat_test_client \
        --network gat_test_network \
-       local/gat_test_client:latest \
-       python /workspace/client.py -m gat_test -u gat_test_server:8001
+       -v`pwd`/client:/inputs \
+       nvcr.io/nvidia/tritonserver:20.06-py3-clientsdk \
+       python /inputs/client.py -m gat_test -u gat_test_server:8001
